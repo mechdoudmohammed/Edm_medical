6465bbe46a62406ae85655f9ded6d2cae7684c7e
mohammed mechdoud <mechdoud.med@gmail.com>
2021-04-05 20:58:30 +0200

    version 5

--- resources/views/backend/fournisseur/index.blade.php
+++ resources/views/backend/fournisseur/index.blade.php
@@ -9,7 +9,7 @@
          </div>
      </div>
     <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary float-left">Fournisseur List</h6>
+      <h6 class="m-0 font-weight-bold text-primary float-left">Fournisseur Liste</h6>
       <a href="{{route('fournisseur.create')}}" class="btn btn-primary btn-sm float-right" data-toggle="tooltip" data-placement="bottom" title="Add User"><i class="fas fa-plus"></i> Add Fournisseur</a>
     </div>
     <div class="card-body">
@@ -24,6 +24,7 @@
               <th>email</th>
               <th>telephone</th>
               <th>description</th>
+              <th>statut</th>
               <th>Action</th>
             </tr>
           </thead>
